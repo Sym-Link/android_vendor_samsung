625a3fc8b94264aa0b557da6e0f2d6330e77ef65
Dorian Snyder <dastin1015@gmail.com>
2013-10-22 23:12:24 -0700

    WIP: d710: open source libril

--- d710/d710-vendor-blobs.mk
+++ d710/d710-vendor-blobs.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := vendor/samsung/d710
 
 # Prebuilt libraries that are needed to build open-source libraries
-PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/proprietary/system/lib/libril.so:obj/lib/libril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:obj/lib/libsecril-client.so
+#PRODUCT_COPY_FILES := \
+#    $(LOCAL_PATH)/proprietary/system/lib/libril.so:obj/lib/libril.so \
+#    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:obj/lib/libsecril-client.so
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/bin/gpsd:system/bin/gpsd \
@@ -25,12 +25,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/bin/macloader:system/bin/macloader \
     $(LOCAL_PATH)/proprietary/system/bin/mfgloader:system/bin/mfgloader \
     $(LOCAL_PATH)/proprietary/system/bin/pppd_runner:system/bin/pppd_runner \
-    $(LOCAL_PATH)/proprietary/system/bin/rild:system/bin/rild \
     $(LOCAL_PATH)/proprietary/system/lib/hw/gps.exynos4.so:system/lib/hw/gps.exynos4.so \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/lib/libakm.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libril.so:system/lib/libril.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsec-ril40.so:system/lib/libsec-ril40.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/lib/libsecril-client.so
+
+#    $(LOCAL_PATH)/proprietary/system/bin/rild:system/bin/rild \
+#$(LOCAL_PATH)/proprietary/system/lib/libril.so:system/lib/libril.so \
 
 #    $(LOCAL_PATH)/proprietary/system/lib/libSECmWiMAXcAPI.so:system/lib/libSECmWiMAXcAPI.so \
 #    $(LOCAL_PATH)/proprietary/system/etc/wimax_boot.bin:system/etc/wimax_boot.bin \
